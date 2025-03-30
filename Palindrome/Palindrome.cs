4558f3b2717d9b233f8ffeeb6feaaf9ec8dd0008
SofiaBurma <sofiburma-ipt28@lll.kpi.ua>
2025-03-30 21:59:48 +0300

    Add project files.

---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,26 @@
+﻿namespace Palindrome
+{
+    public class Palindrome
+    {
+        public int CheckPalindrome(string input)
+        {
+            if (input.Contains(" "))
+            {
+                Console.Error.WriteLine("Input contains spaces.");
+                return 1;
+            }
+
+            string reversed = new string(input.Reverse().ToArray());
+            if (input == reversed)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace lab2
+{
+     public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+            Console.Write("Введіть слово для перевірки: ");
+            input = Console.ReadLine();
+
+            var palindrome = new Palindrome.Palindrome();
+            var result = palindrome.CheckPalindrome(input);
+            Console.WriteLine(result);
+
+        }
+    }
+}
